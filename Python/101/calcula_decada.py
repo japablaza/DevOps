d69c0eae2cf3ef8967ad16586a6316216b144786
Jose Apablaza <japablaza@gmail.com>
2022-09-01 16:20:05 -0500

    Simple calculo de decadas

---
+++ Python/101/calcula_decada.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# This is a simple python script that calculate decades
+
+tu_edad  = input('Que edad tienes? ')
+tu_edad  = int(tu_edad)
+decada   = tu_edad/10
+print(f'Tu tienes {decada} decadas')
