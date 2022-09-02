db76fb84f24e6adee32c58f545029bd1be04f66d
Jose Apablaza <japablaza@gmail.com>
2022-09-02 10:41:25 -0500

    Muestra la decada y anios

--- Python/101/calcula_decada.py
+++ Python/101/calcula_decada.py
@@ -2,7 +2,7 @@
 
 # This is a simple python script that calculate decades
 
-tu_edad  = input('Que edad tienes? ')
-tu_edad  = int(tu_edad)
-decada   = tu_edad/10
-print(f'Tu tienes {decada} decadas')
+tu_edad  = int(input('Que edad tienes? '))
+decada   = tu_edad // 10 # Divide sin coma
+anios    = tu_edad % 10  # Modulo
+print(f'Tu tienes {decada} decadas, y {anios} anios')
