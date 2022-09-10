da6fc541af49facf40d5b4a7949c132b7b9c8ff7
Jose Apablaza <japablaza@gmail.com>
2022-09-09 19:18:27 -0600

    adding list

--- Python/102/open_txt.py
+++ Python/102/open_txt.py
@@ -6,11 +6,14 @@ url = 'https://sixty-north.com/c/t.txt'
 url2 = 'http://cuatrodedos.com/web2.0/'
 
 historia = uo(url)
-
+palabras = []
 for line in historia:
-    print(line.decode('utf-8').split())
+    line = line.decode('utf-8').split()
+    for word in line:
+        palabras.append(word)
+
 # print(historia.read())
 
 historia.close()
 
-
+print(palabras)
