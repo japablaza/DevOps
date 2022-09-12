cd022cf3153d3a47db6634f8aff2f49a8d568b9a
Jose Apablaza <japablaza@gmail.com>
2022-09-12 14:28:31 -0600

    New modules to print main function and by words

--- Python/102/open_txt.py
+++ Python/102/open_txt.py
@@ -2,18 +2,30 @@
 
 from urllib.request import urlopen as uo
 
-url = 'https://sixty-north.com/c/t.txt'
-url2 = 'http://cuatrodedos.com/web2.0/'
 
-historia = uo(url)
-palabras = []
-for line in historia:
-    line = line.decode('utf-8').split()
-    for word in line:
-        palabras.append(word)
+def a_buscar():
+    url = 'https://sixty-north.com/c/t.txt'
+    url2 = 'http://cuatrodedos.com/web2.0/'
 
-# print(historia.read())
+    historia = uo(url)
+    palabras = []
+    for line in historia:
+        line = line.decode('utf-8').split()
+        for word in line:
+            palabras.append(word)
 
-historia.close()
+    historia.close()
+    return palabras
 
-print(palabras)
+
+def imprime_items(items):
+    for item in items:
+        print(item)
+
+
+def main():
+    frace = a_buscar()
+    imprime_items(frace)
+
+if __name__ == '__main__':
+    main()
