3664c48ac45f5b8f76be1b98ca08f4feeaf56c9b
Jose Apablaza <japablaza@gmail.com>
2023-08-10 15:43:58 -0500

    ejemplo de elif

---
+++ Python/101/plantas.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Ejercicio 22
+
+# El usuario te da la informacion
+planta = input("Que planta quieres? :")
+
+name01 = "Spathiphyllum"
+name02 = "spathiphyllum"
+
+if name01 == planta:
+    print("YES")
+elif name02 == planta:
+    print("NO")
+else:
+    print("No quiero " planta)
+    
+    
