b47aaeee8720efec18f65a01318627d049d3bbc4
Jose Apablaza <japablaza@gmail.com>
2023-08-10 08:09:23 -0500

    simple True False

---
+++ Python/101/true_false.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# Simple True False program
+
+# Get your variables ready
+m = int(input("Dame el primer numero: "))
+
+# Check the number with the got
+if m < 100:
+    print("El numero e menor a 100")
+else:
+    print("El numero es mayor a 100")
