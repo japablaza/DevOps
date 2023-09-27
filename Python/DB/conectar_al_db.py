11ed2dbc01357734bc351c74567d6c5efa047f72
Jose Apablaza <japablaza@gmail.com>
2023-09-27 18:48:01 -0500

    Leer variables del ENV

--- Python/DB/conectar_al_db.py
+++ Python/DB/conectar_al_db.py
@@ -3,4 +3,10 @@
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
+import os 
 
+server  = os.environ.get("DB_SERVER")
+usuario = os.environ.get("DB_USER")
+laclave = os.environ.get("DB.CLAVE")
+ 
+def contect_a_db(server, usuario, laclave)
