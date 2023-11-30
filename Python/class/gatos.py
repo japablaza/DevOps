04e48145736e6700ebad6d6d486196a8e6e4708f
Jose Apablaza <japablaza@gmail.com>
2023-11-30 14:24:43 -0600

    Basic Class

---
+++ Python/class/gatos.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class gatos:
+    
+    def __init__(self, color="Negro", sexo="Hembra", edad=3):
+        self.color = color
+        self.sexo = sexo
+        self.edad = edad
+        
+        
+croockshanks = gatos("Naranjo", "Macho", 3)
+        
+print(croockshanks.color)
